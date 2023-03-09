44b63770994efba2db74f451edcb17c6d0a459c6
daydreamer <s419922572@gmail.com>
2023-03-09 15:04:15 +0900

    First Commit

---
+++ src/main/java/com/myspringtutorial/spring6webapptutorial/Spring6WebAppTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.myspringtutorial.spring6webapptutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebAppTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebAppTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myspringtutorial/spring6webapptutorial/Spring6WebAppTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringtutorial.spring6webapptutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebAppTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
