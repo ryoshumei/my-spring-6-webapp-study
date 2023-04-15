739f913afeb25e82e089b52cf354c7ddb855ab25
daydreamer <s419922572@gmail.com>
2023-04-15 20:47:01 +0900

    finished section11 course129

--- src/main/java/com/myspring6_study/spring6restmvc/entities/Customer.java
+++ src/main/java/com/myspring6_study/spring6restmvc/entities/Customer.java
@@ -3,6 +3,8 @@ package com.myspring6_study.spring6restmvc.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -16,7 +18,8 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     UUID id;
     String customerName;
 
